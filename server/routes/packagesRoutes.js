04e5e0051af7176dd3971932bd6422c680245b00
Sanjay Kandula <sanjaykandula3@gmail.com>
2024-11-06 11:44:47 +0530

    umrah update commit

--- server/routes/packagesRoutes.js
+++ server/routes/packagesRoutes.js
@@ -3,5 +3,6 @@ const express=require('express')
 const router=express.Router()
 router.get('/getallcustomer',packageController.getAllCustomers);
 router.post('/addcustomer',packageController.addCustomer);
-router.post('/sendmail',packageController.sendEmail)
+router.post('/sendmail',packageController.sendEmail);
+router.post('/umrahmail',packageController.umrahMail)
 module.exports = router;
